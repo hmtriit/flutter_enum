edae6085e06f58597517ec58cc4e2cec73be8d3e
hmtri <hmtri.it@gmail.com>
2025-06-03 09:20:10 +0700

    Final Enum Starter basic work with my class Order

--- lib/enums/enums.dart
+++ lib/enums/enums.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_enum/order_status_item_view.dart';
 
@@ -14,7 +15,7 @@ class _EnumsState extends State<Enums> {
   @override
   Widget build(BuildContext context) {
     final themeColors = Theme.of(context).colorScheme;
-    MyOrder order = MyOrder.orther;
+    MyOrder order = MyOrder.orther.copyWith(status: OrderStatusEnum.delivered);
     return Scaffold(
       backgroundColor: themeColors.background,
       appBar: AppBar(
@@ -40,30 +41,41 @@ class _EnumsState extends State<Enums> {
           bottom: 16,
         ),
         children: [
-          OrderStatusItemView(
-            color: OrderStatusEnum.processing.color,
-            title: OrderStatusEnum.processing.title,
-            subtitle: OrderStatusEnum.processing.description,
-            icon: OrderStatusEnum.processing.icon,
-            showLine: true,
-            isActive: order.status == OrderStatusEnum.processing,
-          ),
-          OrderStatusItemView(
-            color: OrderStatusEnum.inTransit.color,
-            title: OrderStatusEnum.inTransit.title,
-            subtitle: OrderStatusEnum.inTransit.description,
-            icon: OrderStatusEnum.inTransit.icon,
-            showLine: true,
-            isActive: order.status == OrderStatusEnum.inTransit,
-          ),
-          OrderStatusItemView(
-            color: OrderStatusEnum.delivered.color,
-            title: OrderStatusEnum.delivered.title,
-            subtitle: OrderStatusEnum.delivered.description,
-            icon: OrderStatusEnum.delivered.icon,
-            showLine: false,
-            isActive: order.status == OrderStatusEnum.delivered,
-          ),
+
+          ...OrderStatusEnum.values.
+          mapIndexed((i, e) => OrderStatusItemView(
+              color: e.color,
+              title: e.title,
+              subtitle: e.description,
+              icon: e.icon,
+              showLine: i < OrderStatusEnum.values.length - 1,
+              isActive: OrderStatusEnum.values.indexOf(order.status) >= i),
+              ).toList(),
+
+          // OrderStatusItemView(
+          //   color: OrderStatusEnum.processing.color,
+          //   title: OrderStatusEnum.processing.title,
+          //   subtitle: OrderStatusEnum.processing.description,
+          //   icon: OrderStatusEnum.processing.icon,
+          //   showLine: true,
+          //   isActive: order.status == OrderStatusEnum.processing,
+          // ),
+          // OrderStatusItemView(
+          //   color: OrderStatusEnum.inTransit.color,
+          //   title: OrderStatusEnum.inTransit.title,
+          //   subtitle: OrderStatusEnum.inTransit.description,
+          //   icon: OrderStatusEnum.inTransit.icon,
+          //   showLine: true,
+          //   isActive: order.status == OrderStatusEnum.inTransit,
+          // ),
+          // OrderStatusItemView(
+          //   color: OrderStatusEnum.delivered.color,
+          //   title: OrderStatusEnum.delivered.title,
+          //   subtitle: OrderStatusEnum.delivered.description,
+          //   icon: OrderStatusEnum.delivered.icon,
+          //   showLine: false,
+          //   isActive: order.status == OrderStatusEnum.delivered,
+          // ),
         ],
       ),
     );
@@ -95,6 +107,12 @@ enum OrderStatusEnum {
       title: 'Processing',
       description: 'Your order is being processed.',
       icon: Icons.hourglass_top_outlined
+  ),
+  packaging(
+      color: Colors.indigo,
+      title: 'Packaging',
+      description: 'Your order is being Packaged.',
+      icon: Icons.touch_app_outlined
   ),
   inTransit(
       color: Colors.blue,
